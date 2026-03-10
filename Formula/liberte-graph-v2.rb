c25c7623889fd2004cf96a652ab5fa3fb40d2cc1
Chaykin <chay@Mac.lan>
2026-03-10 10:33:56 +0100

    Fix liberte-graph-v2 brew audit npm std_npm_args warning

--- Formula/liberte-graph-v2.rb
+++ Formula/liberte-graph-v2.rb
@@ -14,8 +14,7 @@ class LiberteGraphV2 < Formula
   depends_on "node" => :build
 
   def install
-    system "npm", "install", *std_npm_args
-    system "npm", "install", "--include=dev"
+    system "npm", "install", "--include=dev", *std_npm_args
     system "npm", "run", "build"
 
     libexec.install "dist"
