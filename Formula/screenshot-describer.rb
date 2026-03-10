ba2a6cc1defd97e962002a68cac75cbd79da4f65
Chaykin <chay@Mac.lan>
2026-03-10 11:18:44 +0100

    Bump screenshot-describer to v0.1.8

--- Formula/screenshot-describer.rb
+++ Formula/screenshot-describer.rb
@@ -1,8 +1,8 @@
 class ScreenshotDescriber < Formula
   desc "Menubar service that watches a folder and processes new files"
   homepage "https://github.com/avchaykin/screenshot-describer"
-  url "https://github.com/avchaykin/screenshot-describer/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "eb2effd412ccf89278bca5c18b89a6505144f102f5f7622bc469bc52afe5a9c1"
+  url "https://github.com/avchaykin/screenshot-describer/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "25c6d4a8fe272d4be497d9ccadaa80e15feb0a7cb4b7fbcd00d65837f7fa5d8b"
   license "MIT"
   head "https://github.com/avchaykin/screenshot-describer.git", branch: "main"
 
